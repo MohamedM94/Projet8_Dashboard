f67e3fa9ad0c53f8306c64ec15210b15cc291726
MohamedM94 <166175173+MohamedM94@users.noreply.github.com>
2024-12-27 13:03:21 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -101,7 +101,8 @@ def main() :
                 #Title display
     html_temp = """
     <div style="background-color: #D54773; padding:10px; border-radius:10px">
-    <h1 style="color: white; text-align:center">Dashboard Scoring Credit\n  
+    <h1 style="color: white; text-align:center">Dashboard Scoring Credit\n 
+     st.header("MERAH Mohamed ")
     MERAH Mohamed</h1>
     </div>
 
